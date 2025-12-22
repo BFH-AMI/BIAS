d89625747b5ede2fa9dd1153aa6df1bfdfe61901
Mascha Kurpicz-Briki <mascha@Strawberry.local>
2025-12-22 09:52:08 +0100

    Moving contents to more specific repository. This becomes the overview page.

--- BiasDetection/config.py
+++
@@ -1,6 +0,0 @@
-FOLDER_PATH = "models/"
-
-FASTTEXT_FILE= "cc.XX.300.bin"
-WORD2VEC_FILE = "GoogleNews-vectors-negative300.bin"
-GLOVE_FILE = "glove.840B.300d.txt"
-mBERT_FILE = "google-bert/bert-base-multilingual-cased"
